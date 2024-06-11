adb4dcd5ffc4876251aea910f741dd5b127b973c
vivek <vivekpande074@gmail.com>
2024-06-12 05:05:37 +0530

    minor bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api':
-    {target:'http://localhost:5000',
+    {target:'https://retrade-7ttv.onrender.com',
     changeOrigin: true,
     secure: false,
     }
