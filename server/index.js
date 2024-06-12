00dae4167fadcb0430a06b75d85d65a6ca151053
vivek <vivekpande074@gmail.com>
2024-06-12 11:53:39 +0530

    minor change

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const cors=require("cors");
   
 
 const corsOptions = {
-  origin:'http://localhost:5173',
+  origin:'https://retrade-1.onrender.com',
   credential:true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Authorization', 'Content-Type'],
